11da236ce484ba8c187485cbca5505f7ebef6788
Johnny Console <johnny.console@hotmail.com>
2024-02-01 15:30:30 -0500

    Add custom adapter to initialze rotor spinners

--- app/src/main/java/net/johnnyconsole/android/enigma/MainActivity.kt
+++ app/src/main/java/net/johnnyconsole/android/enigma/MainActivity.kt
@@ -1,11 +1,38 @@
 package net.johnnyconsole.android.enigma
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.appcompat.content.res.AppCompatResources
+import net.johnnyconsole.android.enigma.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private class RotorAdapter(context: Context, val array: Array<String>): ArrayAdapter<String>(context, R.layout.layout_spinner) {
+        override fun getCount(): Int {
+            return array.size
+        }
+
+        override fun getItem(position: Int): String? {
+            return array[position]
+        }
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = RotorAdapter(this, resources.getStringArray(R.array.alphabet))
+
+        binding.RotorL!!.adapter = adapter
+        binding.RotorM!!.adapter = adapter
+        binding.RotorR!!.adapter = adapter
+
     }
 }
