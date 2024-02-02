a1daf43720ad63b648616ee7e7a95e70dbd8568b
Johnny Console <johnny.console@hotmail.com>
2024-02-02 08:07:05 -0500

    Begin Adding Enigma Logic to MainActivity

--- app/src/main/java/net/johnnyconsole/android/enigma/MainActivity.kt
+++ app/src/main/java/net/johnnyconsole/android/enigma/MainActivity.kt
@@ -5,7 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import net.johnnyconsole.android.enigma.algorithm.Enigma
+import net.johnnyconsole.android.enigma.algorithm.Plugboard
 import net.johnnyconsole.android.enigma.databinding.ActivityMainBinding
+import net.johnnyconsole.android.enigma.algorithm.Reflector.Companion.A
+import net.johnnyconsole.android.enigma.algorithm.Reflector.Companion.B
+import net.johnnyconsole.android.enigma.algorithm.Reflector.Companion.C
+import net.johnnyconsole.android.enigma.algorithm.Rotor.Companion.I
+import net.johnnyconsole.android.enigma.algorithm.Rotor.Companion.II
+import net.johnnyconsole.android.enigma.algorithm.Rotor.Companion.III
+import net.johnnyconsole.android.enigma.algorithm.Rotor.Companion.IV
+import net.johnnyconsole.android.enigma.algorithm.Rotor.Companion.V
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,17 +24,19 @@ class MainActivity : AppCompatActivity() {
             return array.size
         }
 
-        override fun getItem(position: Int): String? {
+        override fun getItem(position: Int): String {
             return array[position]
         }
     }
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var enigma: Enigma
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enigma = Enigma(Plugboard(arrayOf("AB", "CD", "EF", "GH", "IJ", "KL", "MN", "OP", "QR", "ST")), I, II, III, A)
 
         val adapter = RotorAdapter(this, resources.getStringArray(R.array.alphabet))
 
@@ -32,6 +44,16 @@ class MainActivity : AppCompatActivity() {
         binding.RotorM.adapter = adapter
         binding.RotorR.adapter = adapter
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("enigma", enigma)
+    }
+
+    override fun onRestoreInstanceState(inState: Bundle) {
+        super.onRestoreInstanceState(inState)
+        enigma = inState.getSerializable("enigma", Enigma::class.java)!!
     }
 
     @SuppressWarnings("discouragedApi")
