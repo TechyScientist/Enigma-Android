40ccde7d98210386a927b44d3c183bfbb9345d7c
Johnny Console <johnny.console@hotmail.com>
2024-02-05 10:19:56 -0500

    EnigmaSettingsActivity prompts users for confirmation before exiting

--- app/src/main/java/net/johnnyconsole/android/enigma/EnigmaSettingsActivity.kt
+++ app/src/main/java/net/johnnyconsole/android/enigma/EnigmaSettingsActivity.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import net.johnnyconsole.android.enigma.databinding.ActivityEnigmaSettingsBinding
 
 class EnigmaSettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEnigmaSettingsBinding
 
-    private class RotorAdapter(context: Context, val array: Array<String>): ArrayAdapter<String>(context, R.layout.layout_spinner) {
+    private class RotorAdapter(context: Context, val array: Array<String>) :
+        ArrayAdapter<String>(context, R.layout.layout_spinner) {
         override fun getCount(): Int {
             return array.size
         }
@@ -19,6 +23,7 @@ class EnigmaSettingsActivity : AppCompatActivity() {
             return array[position]
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEnigmaSettingsBinding.inflate(layoutInflater)
@@ -35,5 +40,25 @@ class EnigmaSettingsActivity : AppCompatActivity() {
         binding.RingM.adapter = rotorAdapter
         binding.RingR.adapter = rotorAdapter
         binding.Reflector.adapter = reflectorAdapter
+
+        binding.btBack.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.exit_confirm_title)
+                .setMessage(R.string.exit_confirm)
+                .setPositiveButton(getString(R.string.yes)) { _, _ -> finish() }
+                .setNegativeButton(R.string.no, null)
+                .create().show()
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                AlertDialog.Builder(this@EnigmaSettingsActivity)
+                    .setTitle(R.string.exit_confirm_title)
+                    .setMessage(R.string.exit_confirm)
+                    .setPositiveButton(getString(R.string.yes)) { _, _ -> finish() }
+                    .setNegativeButton(R.string.no, null)
+                    .create().show()
+            }
+        })
     }
 }
