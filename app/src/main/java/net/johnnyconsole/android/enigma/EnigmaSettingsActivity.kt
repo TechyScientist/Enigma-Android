c24aa832ed3f1818072e3e4a82c1f61eb2a961c8
Johnny Console <johnny.console@hotmail.com>
2024-02-05 09:51:49 -0500

    Add Basic Layout for EnigmaSettingsActivity

--- app/src/main/java/net/johnnyconsole/android/enigma/EnigmaSettingsActivity.kt
+++ app/src/main/java/net/johnnyconsole/android/enigma/EnigmaSettingsActivity.kt
@@ -1,15 +1,39 @@
 package net.johnnyconsole.android.enigma
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import net.johnnyconsole.android.enigma.databinding.ActivityEnigmaSettingsBinding
 
 class EnigmaSettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEnigmaSettingsBinding
+
+    private class RotorAdapter(context: Context, val array: Array<String>): ArrayAdapter<String>(context, R.layout.layout_spinner) {
+        override fun getCount(): Int {
+            return array.size
+        }
+
+        override fun getItem(position: Int): String {
+            return array[position]
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEnigmaSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar!!.title = getString(R.string.settings)
+
+        val rotorAdapter = RotorAdapter(this, resources.getStringArray(R.array.alphabet))
+        val reflectorAdapter = RotorAdapter(this, arrayOf("A", "B", "C"))
+
+        binding.RotorL.adapter = rotorAdapter
+        binding.RotorM.adapter = rotorAdapter
+        binding.RotorR.adapter = rotorAdapter
+        binding.RingL.adapter = rotorAdapter
+        binding.RingM.adapter = rotorAdapter
+        binding.RingR.adapter = rotorAdapter
+        binding.Reflector.adapter = reflectorAdapter
     }
 }
